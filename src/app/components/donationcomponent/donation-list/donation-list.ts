16a723d1402c874cdebbf2a2e9a894b4327058d3
MarcoChavezLoli <u201917180@upc.edu.pe>
2026-06-30 23:07:42 -0500

    Avance Donation List

--- src/app/components/donationcomponent/donation-list/donation-list.ts
+++ src/app/components/donationcomponent/donation-list/donation-list.ts
@@ -1,11 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Donation } from '../../../models/donation';
+import { ItemDTO } from '../../../models/itemDTO';
+import { Usuario } from '../../../models/usuario';
+import { Donationservice } from '../../../services/donationservice';
+import { ItemService } from '../../../services/itemservice';
+import { Usuarioservice } from '../../../services/usuarioservice';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-donation-list',
-  imports: [],
+  imports: [MatTableModule, MatPaginatorModule, MatIconModule, RouterLink],
   templateUrl: './donation-list.html',
   styleUrl: './donation-list.css',
 })
-export class DonationList {
+export class DonationList implements OnInit {
 
+  dataSource: MatTableDataSource<Donation> = new MatTableDataSource();
+  displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7'];
+  items: ItemDTO[] = [];
+  usuario: Usuario[] = [];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(
+    private dS: Donationservice,
+    private iS: ItemService,
+    private uS: Usuarioservice,
+  ) { }
+
+  ngOnInit(): void {
+    this.iS.list().subscribe(data => {
+      this.items = data;
+    });
+    this.uS.list().subscribe(data => {
+      this.usuario = data;
+    });
+    this.cargarDonaciones();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  cargarDonaciones() {
+    this.dS.list().subscribe({
+      next: (data: Donation[]) => {
+        this.dataSource.data = data;
+      },
+      error: () => {
+        this.dataSource.data = [];
+      },
+    });
+  }
+  getitems(id: number): string {
+    return (
+      this.items.find(item => item.itemId === id)?.itemName || 'Sin nombre'
+    );
+  }
+
+  getUsuario(id: number): string {
+    const usuario = this.usuario.find(u => u.idUser === id);
+    if (!usuario) {
+      return 'Sin usuario';
+    }
+    return `${usuario.userName} ${usuario.userLastName}`;
+  }
 }
--- src/app/components/donationcomponent/donationcomponent.ts
+++ src/app/components/donationcomponent/donationcomponent.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { DonationList } from './donation-list/donation-list';
 
 @Component({
   selector: 'app-donationcomponent',
-  imports: [],
+  imports: [RouterOutlet, DonationList],
   templateUrl: './donationcomponent.html',
   styleUrl: './donationcomponent.css',
 })
 export class Donationcomponent {
+
+  constructor(public route: ActivatedRoute) {}
 
 }
