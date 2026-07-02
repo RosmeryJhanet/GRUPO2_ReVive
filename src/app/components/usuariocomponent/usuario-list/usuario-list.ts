699b4a0036c8d87a9ea2c6c6b157a0c9182fbd1b
LuisOrtiz30 <yor30112001@gmail.com>
2026-07-02 03:03:55 -0500

    agregando card a lista de usuarios

--- src/app/components/usuariocomponent/usuario-list/usuario-list.ts
+++ src/app/components/usuariocomponent/usuario-list/usuario-list.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { MatTableModule, MatTableDataSource } from '@angular/material/table';
-import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
-import { Usuarioservice } from '../../../services/usuarioservice';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { MatDividerModule } from '@angular/material/divider';
+import { Usuarioservice } from '../../../services/usuarioservice';
 import { Usuario } from '../../../models/usuario';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatDialog } from '@angular/material/dialog';
@@ -12,39 +13,33 @@ import { Confirmdialogcomponent } from '../../confirmdialogcomponent/confirmdial
 
 @Component({
   selector: 'app-usuario-list',
-  imports: [MatTableModule, MatIconModule, RouterLink, MatPaginatorModule, CommonModule],
+  imports: [MatIconModule, MatCardModule, MatButtonModule, RouterLink, CommonModule, MatDividerModule],
   templateUrl: './usuario-list.html',
   styleUrl: './usuario-list.css',
 })
-export class UsuarioList implements OnInit, AfterViewInit {
-  dataSource: MatTableDataSource<Usuario> = new MatTableDataSource();
-  displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8'];
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
+export class UsuarioList implements OnInit {
+  usuarios: Usuario[] = [];
 
   constructor(
     private uS: Usuarioservice,
     private snackBar: MatSnackBar,
     private dialog: MatDialog,
+    private cdr: ChangeDetectorRef,
   ) {}
 
   ngOnInit(): void {
     this.cargarUsuarios();
   }
 
-  ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator;
-  }
-
   cargarUsuarios() {
     this.uS.list().subscribe({
       next: (data) => {
-        this.dataSource.data = data;
+        this.usuarios = data;
+        this.cdr.detectChanges();
       },
       error: (err) => {
-        if (err.status === 404) {
-          this.dataSource.data = [];
-        }
+        if (err.status === 404) this.usuarios = [];
+        this.cdr.detectChanges();
       },
     });
   }
